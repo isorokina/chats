0c0ce2163df25e34de30402ff19503966ad446b9
isorokina <ijasorokina@inbox.lv>
2022-03-18 15:37:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function suutiitZinju()
     ///let zinjas=document.querySelector(.chataZinjas);
 
     zinjas.innerHTML=zinjas.innerHTML+'</br>'+zinja.value;
-   // fetch(API + '/suutiit/Juris/' + zinja.value)
+    fetch(API + '/suutiit/' +vaards.value+ '/'+ zinja.value)//otrā kursu daļa
 }
 async function ielaadeetChataZinjas()
 {
@@ -21,4 +21,20 @@ async function ielaadeetChataZinjas()
     //console.log(dati);
     zinjas.innerHTML=dati;
 }
-setInterval(ielaadeetChataZinjas,1000)//ielādēt 1 sek čata ziņas
+//setInterval(ielaadeetChataZinjas,1000)//ielādēt 1 sek čata ziņas
+async function ielaadeetChataZinjasJson()
+{
+    ///let datiNoServera=await fetch('chatazinjas.txt')
+    let datiNoServera=await fetch(API+'/lasiit')
+    let dati=await datiNoServera.json();
+    //console.log(await dati);
+    //zinjas.innerHTML=dati;
+    //var i=i//plašāks, pēc noklusējuma
+    i=0;
+    while(i< 5))
+    {
+        console.log(i)
+        i=i+1;
+    }
+}
+
