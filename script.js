6bb548d481236fb704aab0785ac1a5ed96da309b
isorokina <ijasorokina@inbox.lv>
2022-03-18 11:17:48 +0200

    lab4

--- script.js
+++ script.js
@@ -2,4 +2,9 @@
 function suutiitZinju()
 {
     console.log("Sūtīt ziņu darbojās");
+    //let zinja=document.querySelector(.manaZinja).value; //piešķir vienu vērtējumu
+    let zinja=document.querySelector(.manaZinja);
+    let zinjas=document.querySelector(.chataZinjas);
+
+    zinjas.innerHTML='koko';
 }
